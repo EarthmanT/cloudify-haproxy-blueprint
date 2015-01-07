2d8295ae8f14be528d119b1b95107c4e6fea56f8
earthmant <trammell.earthling@gmail.com>
2015-01-07 17:48:58 +0200

    starts haproxy daemon

---
+++ scripts/start-ubuntu.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e
+
+ctx logger debug "Setting ENABLED=1 in /etc/default/haproxy"
+
+/bin/sed -i 's/ENABLED=0/ENABLED=1/' /etc/default/haproxy || exit 1
+
+COMMAND="/etc/init.d/haproxy start"
+
+logger info "Starting HAProxy"
+ctx logger debug "${COMMAND}"
+
+er=1
+while [[ $er != 0 ]]
+do
+    nohup ${COMMAND} /dev/null 2>&1 &
+    er=$?
+    sleep 10
+done
+ctx logger info "Started HAProxy"
