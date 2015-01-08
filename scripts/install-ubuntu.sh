a81846380de600b34270b068469d99223e3250d9
earthmant <trammell.earthling@gmail.com>
2015-01-08 15:00:48 +0200

    I wrote udpate instead of update.

--- scripts/install-ubuntu.sh
+++ scripts/install-ubuntu.sh
@@ -5,7 +5,7 @@ set -e
 ctx logger info "Installing HAProxy"
 ctx logger debug "${COMMAND}"
 
-sudo apt-get udpate || exit 1
+sudo apt-get update || exit 1
 sudo apt-get -y install haproxy || exit 1
 
 ctx logger info "Installed HAProxy"
